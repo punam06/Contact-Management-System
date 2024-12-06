82647698a6286dd6555ddb5e8b2c35b8e6200ba6
punam06 <144909093+punam06@users.noreply.github.com>
2024-12-06 12:58:15 +0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,37 +1,36 @@
-from manager import add_contact, view_contact,remove_contact,search_contact
+from manager import add_contact,view_contact,remove_contact,search_contact
 
+def menu():
+    while True:
+        print("\n\n Welcome to  CONTACT MANAGEMENT APP ")
+        print(" 1. Add Contact: ")
+        print(" 2. View Contact: ")
+        print(" 3. Remove Contact: ")
+        print(" 4. Search Contact: ")
+        print(" 5. Exit")
 
-while True:
-    print("\n\n Welcome to  CONTACT MANAGEMENT APP ")
-    print(" 1. Add Contact: ")
-    print(" 2. View Contact: ")
-    print(" 3. Remove Contact: ")
-    print(" 4. Search Contact: ")
-    print(" 5. Exit")
+        choice = input("Enter your choice: ").strip()
+        if choice == "1":
+            name = input("Enter name : ")
+            email = input("Enter email: ")
+            contact_no = input("Enter contact number: ")
+            address = input("Enter your address: ")
+            add_contact(name, email, contact_no, address)
+            print("Task added Successfully!")
 
-    choice = input("Enter your choice: ").strip()
-    if choice == "1":
-        first_name=input("Enter your first name: ")
-        last_name=input("Enter your last name: ")
-        email=input("Enter email Adress: ")
-        contact_no=input("Enter Contact Number: ")
-        adress=input("Enter your adress: ")
-        add_contact(first_name,last_name,email,contact_no,adress)
-        print("Task added Successfully!")
-    
-    elif choice == "2":
-        view_contact()
-    elif choice == "3":
-        index=int(input("Enter the contact indext to remove: "))
-        remove_contact(index)
-    elif choice == "4":
-        query= input("Enter the search query: ")
-        print("\nSearch Results: ")
-        search_contact(query)
-        
-    elif choice == "5":
+        elif choice == "2":
+            view_contact()
+        elif choice == "3":
+            name = input("Enter number: ")
+            remove_contact(name)
+        elif choice == "4":
+            query = input("Enter the search query: ")
+            search_contact(query)
+        elif choice == "5":
             print("Exiting the program. Goodbye!")
-            break  # Exit the loop and terminate the program
-    else:
-     print("Invalid choice. Please try again.")
-    
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    menu()
